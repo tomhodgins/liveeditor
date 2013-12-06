3c9a60e4bebe98cbacfbfbaba83bec3e9bd3cb3b
tomhodgins <tomhodgins@gmail.com>
2013-12-06 12:48:43 -0500

    Removing unneeded characters
    Suggested edit from github user: xem! :)

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   onload=(document).onkeyup=function(){
     (document.getElementById("preview").contentWindow.document).write(
-      html["value"]+"<style>"+css["value"]+"<\/style><script>"+js["value"]+"<\/script>"
+      html.value+"<style>"+css.value+"<\/style><script>"+js.value+"<\/script>"
     );
     (document.getElementById("preview").contentWindow.document).close()
   };
