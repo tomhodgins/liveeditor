202ec4c596ecbba436ee8d7930488a5d6bc8b521
Tom Hodgins <tomhodgins@gmail.com>
2013-12-06 12:07:53 -0500

    Adding files to Git

---
+++ README.md
@@ -0,0 +1,4 @@
+Live Editor
+========
+
+Live Editor is an simple in-browser HTML editor that allows you to write HTML, CSS, and JavaScript in realtime and preview the results on the same page.
---
+++ js/editor.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  onload=(document).onkeyup=function(){
+    (document.getElementById("preview").contentWindow.document).write(
+      html["value"]+"<style>"+css["value"]+"<\/style><script>"+js["value"]+"<\/script>"
+    );
+    (document.getElementById("preview").contentWindow.document).close()
+  };
+});
